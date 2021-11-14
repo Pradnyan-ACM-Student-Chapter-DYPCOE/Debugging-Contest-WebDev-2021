e5fe7667476d06651471b26ea69f3d4ca3b75e47
AmeyaJain-25 <ameiya.jain@gmail.com>
2021-11-14 19:27:51 +0530

    Solution Code

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,11 +5,22 @@ const AddNote = ({ handleAddNote }) => {
   const characterLimit = 200;
 
   const handleChange = (event) => {
-    setNoteText(event.target.value);
+    //BUG7: Start - On adding a new note, the count is showing NaN. And also if I try to write characters more than 200, it is able to write rather than not exceeding it from 200.
+    if (characterLimit - event.target.value.length >= 0) {
+      setNoteText(event.target.value);
+    }
+    // setNoteText(event.target.value);
+    //BUG7: End
   };
 
   const handleSaveClick = () => {
-    handleAddNote(noteText);
+    //BUG8: Start - You cannot add empty notes and also, after adding a note, the previous note data is not removed.
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText("");
+    }
+    // handleAddNote(noteText);
+    //BUG8: End
   };
 
   return (
@@ -22,8 +33,10 @@ const AddNote = ({ handleAddNote }) => {
         onChange={handleChange}
       ></textarea>
       <div className="note-footer" style={{ flexDirection: "row" }}>
-        <small>{characterLimit - noteText} Remaining</small>
-
+        {/* BUG7: Start  */}
+        <small>{characterLimit - noteText.length} Remaining</small>
+        {/* <small>{characterLimit - noteText} Remaining</small> */}
+        {/* BUG7: End */}
         <button className="save" onClick={handleSaveClick}>
           Save
         </button>
--- src/components/Note.js
+++ src/components/Note.js
@@ -9,15 +9,32 @@ const Note = ({ id, text, date, handleDeleteNote, handleEditNote }) => {
   const characterLimit = 200;
 
   const handleChange = (event) => {
-    setNoteText(event.target.value);
+    //BUG7: Start
+    if (characterLimit - event.target.value.length >= 0) {
+      setNoteText(event.target.value);
+    }
+    // setNoteText(event.target.value);
+    //BUG7: End
   };
+
+  function longDateStr(Str) {
+    var date = new Date(Str);
+    date = date.toDateString();
+    date =
+      date.substring(0, 4) +
+      "," +
+      date.substring(4, 7) +
+      " " +
+      date.substring(8);
+    return date;
+  }
 
   return (
     <div className="note">
       {!editMode && <span>{text}</span>}
       {editMode && (
         <textarea
-          rows="8"
+          rows="6"
           cols="10"
           value={noteText}
           onChange={handleChange}
@@ -25,27 +42,49 @@ const Note = ({ id, text, date, handleDeleteNote, handleEditNote }) => {
       )}
       <div className="note-footer">
         <div className="editFoot">
-          {editMode && <small>{characterLimit - noteText} Remaining</small>}
+          {/* BUG7: Start */}
+          {editMode && (
+            <small>{characterLimit - noteText.length} Remaining</small>
+          )}
+          {/* {editMode && <small>{characterLimit - noteText} Remaining</small>} */}
+          {/* BUG7: End */}
           {editMode && (
             <button
               className="editBtn"
               onClick={() => {
-                handleEditNote(noteText, id);
+                //BUG9: Start - It allows empty text to be added.
+                if (!noteText) {
+                  return;
+                }
+                //BUG9: End - Nothing to add.. Just remove the above code
+
+                //BUG3: Start
+                handleEditNote(id, noteText);
+                // handleEditNote(noteText, id);
+                //BUG3: End
                 setEditMode(false);
               }}
+              style={{
+                zIndex: 2,
+                height: 25,
+                width: 50,
+              }}
             >
-              Edit
+              <span className="text">Edit</span>
             </button>
           )}
         </div>
         <div className="editFoot2">
-          <small>{date}</small>
+          <small className="date">{longDateStr(date)}</small>
           <img src={DeleteIcon} onClick={() => handleDeleteNote(id)} />
           {editMode ? (
             <img
               src={CancelIcon}
               onClick={() => {
-                setNoteText("");
+                //BUG8: Start - While editing a note, if i don't want to edit it, then it just clears the text rather than closing the edit text area.
+                setEditMode(false);
+                // setNoteText("");
+                //BUG8: End
               }}
             />
           ) : (
